ee08b1003a582810517af357e16d9bab683fd357
sujith3g <sujith3g@gmail.com>
2014-06-26 18:10:43 +0530

    Fixed Hadnsontable not defined bug

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 * @Author: sg
 * @Date:   2014-06-12 13:51:42
 * @Last Modified by:   sg
-* @Last Modified time: 2014-06-19 03:29:40
+* @Last Modified time: 2014-06-26 18:06:07
 * @Email: sujith3g@gmail.com 
 */
 Package.describe({
@@ -11,4 +11,5 @@ summary: "Handsontable 0.10.5 for meteor"
 Package.on_use(function (api, where) {
 	api.use(['jquery'], 'client');
 	api.add_files(['libs/jquery.handsontable.full.js','libs/jquery.handsontable.full.css'], 'client');
+	api.export("Handsontable");
 });
