1d4e6b83d918bbe1eaf7bc1506865de6542edd20
sujith3g <sujith3g@gmail.com>
2014-09-04 00:29:08 +0530

    Updated to Handsontable 0.11.0-beta3

--- package.js
+++ package.js
@@ -2,11 +2,11 @@
 * @Author: sg
 * @Date:   2014-06-12 13:51:42
 * @Last Modified by:   sg
-* @Last Modified time: 2014-06-26 18:06:07
+* @Last Modified time: 2014-09-04 00:28:16
 * @Email: sujith3g@gmail.com 
 */
 Package.describe({
-summary: "Handsontable 0.10.5 for meteor"
+summary: "Handsontable 0.11.0-beta3 for meteor"
 });
 Package.on_use(function (api, where) {
 	api.use(['jquery'], 'client');
